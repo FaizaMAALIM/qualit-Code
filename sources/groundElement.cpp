f92add482f45e61592f651cf19a956de7c3ab433
SheraDev <sherazade2222@gmail.com>
2023-12-11 20:03:28 +0100

    Ajout des classes abstraites groundElement et character

---
+++ sources/groundElement.cpp
@@ -0,0 +1,14 @@
+#include "groundElement.h"
+#include "iostream"
+
+groundElement::groundElement(const position& pos):d_position{pos}{}
+
+void groundElement::changePosition(const position &pos)
+{
+    d_position=pos;    
+}
+
+position groundElement::getPosition() const
+{
+    return d_position;
+}
