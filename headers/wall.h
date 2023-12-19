4762b67ea00f176211d8477f609a1533e0af3bb5
Sandra9865 <naitatmane.sarah2004@gmail.com>
2023-12-19 20:29:58 +0100

    MAJ wall

---
+++ headers/wall.h
@@ -0,0 +1,14 @@
+#ifndef WALL_H_INCLUDED
+#define WALL_H_INCLUDED
+#include "groundElement.h"
+
+class wall : public groundElement
+{
+    public:
+        wall(const position &p);
+        //void display(const viewManager& view) const override;
+
+};
+
+
+#endif
---
+++ sources/wall.cpp
@@ -0,0 +1,6 @@
+#include "wall.h"
+
+wall::wall(const position &p):groundElement{p}{}
+
+
+
