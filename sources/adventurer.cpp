8fd5f8df968efb0ad4c5f91e28aa1e0e7047beda
yass1258 <yasminelagab04@gmail.com>
2023-12-21 16:18:09 +0100

    MAJ Classe adventurer + test unitaires

--- sources/adventurer.cpp
+++ sources/adventurer.cpp
@@ -1,29 +1,51 @@
 #include "adventurer.h"
+#include "viewManager.h"
+#include "attackManager.h"
 
-adventurer::adventurer(const position &pos):character{pos}{}
+adventurer::adventurer(const position &pos,double swordSolidity,double armorySolidity):character{pos},d_sword{swordSolidity},d_armor{armorySolidity}{}
 
 bool adventurer::hasAmulet() const
 {
     return d_hasAmulet;
 }
 
-void adventurer::attack()
+void adventurer::display(const viewManager& view) const 
 {
-    //GESTIONNAIRE D ATTAQUE
+    view.displayAdventurer(*this);
 }
-
-void adventurer::receiveAttack()
-{
-    //GESTIONNAIRE D ATTAQUE
-}
-
-void adventurer::moveAt(const position &pos)
-{
-    //GESTIONNAIRE DE DEPLACEMENT
-}
-
 
 void adventurer::setAmuletTrue()
 {
     d_hasAmulet = true;
+}
+
+sword adventurer::getSword() const
+{
+    return d_sword;
+}
+
+armory adventurer::getArmor() const
+{
+    return d_armor;
+}
+
+
+void adventurer::setSwordSolidity(double sol)
+{
+    d_sword.setSolidity(sol);
+}
+
+void adventurer::setArmorSolidity(double sol)
+{
+    d_armor.setSolidity(sol);
+}
+
+bool adventurer::isOutWithAmulet() const
+{
+    return d_isOut;
+}
+
+void adventurer::setIsOutTrue() 
+{
+    d_isOut = true;
 }
--- test/testadventurer.cpp
+++ test/testadventurer.cpp
@@ -1,10 +1,10 @@
 #ifndef TESTADVENTURER_H
 #define TESTADVENTURER_H
 
-#include "doctest.h"
-#include "position.h"
-#include "adventurer.h"
 
+#include "doctest.h"
+#include "adventurer.h"
+#include "position.h"
 
 TEST_CASE("Test de la classe adventurer")
 {
@@ -17,13 +17,26 @@ TEST_CASE("Test de la classe adventurer")
         REQUIRE_EQ(a.getPosition().getLine(),i);
         REQUIRE_EQ(a.getPosition().getColumn(),j);
     }
-    SUBCASE("Test a l'amulette")
+    SUBCASE("Test possession amulette")
     {
         a.setAmuletTrue();
         REQUIRE_EQ(a.hasAmulet(),true);
     }
+    SUBCASE("Test set épée")
+    {
+        a.setSwordSolidity(10);
+        REQUIRE_EQ(a.getSword().getSolidity(),10);
+    }
+    SUBCASE("Test set armure")
+    {
+        a.setArmorSolidity(10);
+        REQUIRE_EQ(a.getArmor().getSolidity(),10);
+    }
+    SUBCASE("Test is out")
+    {
+        a.setIsOutTrue();
+        REQUIRE_EQ(a.isOutWithAmulet(),true);
+    }
 }
-
-
 
 #endif
