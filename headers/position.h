2c8cd11b12a674cfa019e9c041a934192e5a187f
yass1258 <yasminelagab04@gmail.com>
2023-12-21 16:33:39 +0100

    MAJ position

--- headers/position.h
+++ headers/position.h
@@ -6,9 +6,12 @@ class position
     public:
         position();
         position(int l,int c);
+        position& operator=(const position& pos);
+        bool operator==(const position& pos) const;
         int getLine() const;
         int getColumn() const;
-    private:
+        void setPosition(int line,int col);
+    public:
         int d_line,d_column;
 };
 
