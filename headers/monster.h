cc76d4e7256f24f681ce3ecc8d2c13a1cedb254d
sheraDev <scherazade2222@gmail.com>
2023-12-21 16:25:36 +0100

    correction classe abstraire monster

--- headers/monster.h
+++ headers/monster.h
@@ -2,15 +2,18 @@
 #define MONSTER_H_INCLUDED
 #include "character.h"
 #include "groundElement.h"
+class viewManager;
+class adventurer;
+class ground;
 
 class monster : public character
 {
     public:
         virtual ~monster()=default;
-        monster(const position &pos);
-        void attack() override;
-        void receiveAttack() override;
+        monster(const position &pos, double hability=0.8);
+        void display(const viewManager& view) const override;
         double getHability() const;
+        bool isAtOneCaseAdv(ground &g);
     private:
         double d_hability;
    
--- sources/monster.cpp
+++ sources/monster.cpp
@@ -1,20 +1,32 @@
 #include "monster.h"
+#include "viewManager.h"
 #include <iostream>
+#include "attackManager.h"
+#include "ground.h"
         
-monster::monster(const position &pos) : character{pos} {}
+monster::monster(const position &pos, double hability) : character{pos},d_hability{hability} {}
 
-void monster::attack()
+bool monster::isAtOneCaseAdv(ground &g)
 {
-    //LANCE UNE ATTAQUE : APPELLE LE GESTIONNAIRE D ATTAQUE
+    int lineAdv = g.getAdventurerPosition().getLine();
+    int colAdv = g.getAdventurerPosition().getColumn();
+    int lineMonster = getPosition().getLine();
+    int colMonster = getPosition().getColumn();
+
+    int difLine=std::abs(lineMonster-lineAdv);
+    int difCol =std::abs(colMonster-colAdv);
+   
+    return difLine<=1 && difCol<=1;
 }
 
-void monster::receiveAttack()
-{
-    //RECOIT UNE ATTAQUE : APPELLE LE GESTIONNAIRE D ATTAQUE
-}
 
 double monster::getHability() const
 {
     return d_hability;
 }
 
+void monster::display(const viewManager& view) const
+{
+    //APPEL LAFFICHEUR QUI LAFFICHE (passer lafficheur en parametre)
+    view.displayMonster(*this);
+}
