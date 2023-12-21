1f5a7a7265464eb691652179c648c559be8f15b5
yass1258 <yasminelagab04@gmail.com>
2023-12-21 15:43:26 +0100

    MAJ classe charactere

--- headers/character.h
+++ headers/character.h
@@ -2,6 +2,8 @@
 #define CHARACTER_H_INCLUDED
 #include "groundElement.h"
 #include "position.h"
+class attackManager;
+class ground;
 
 //CLASS REPRESENTANT UN PERSONNAGE DU TERRAIN ( monstres ou aventurier )
 
@@ -11,15 +13,19 @@ class character : public groundElement
     public:
         character(const position &p);
         virtual ~character()= default;
-        virtual void attack()=0;
-        virtual void receiveAttack()=0;
-        virtual void moveAt(const position&p)=0;
+        
+        //GETTERS
         int forcePoints() const;
         int lifePoints() const;
+
+        //SETTERS
+        void setForcePoints(double nouvForce);
+        void setLifePoints(double nouvLife);
+
     
     private:
-        int d_forcePoints;
-        int d_lifePoints;
+        int d_forcePoints=100;
+        int d_lifePoints=1000;
 
 };
 
--- sources/character.cpp
+++ sources/character.cpp
@@ -1,5 +1,6 @@
 #include "character.h"
 
+
 character::character(const position &p):groundElement{p}{}
 
 
@@ -13,3 +14,15 @@ int character::lifePoints() const
 {
     return d_lifePoints;
 }
+
+void character::setForcePoints(double nouvForce)
+{
+    d_forcePoints = nouvForce;
+}
+        
+        
+void character::setLifePoints(double nouvLife)
+{
+    d_lifePoints = nouvLife;
+}
+
