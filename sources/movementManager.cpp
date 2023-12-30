06ff41e6ff53f616b2e913157a7660f3673ad834
sheraDev <scherazade2222@gmail.com>
2023-12-30 09:44:29 +0100

    definition class abstraite gestion mouvements

---
+++ sources/movementManager.cpp
@@ -0,0 +1,11 @@
+ #include "movementManager.h"
+#include "ground.h"
+
+movementManager::movementManager(const position&pos):d_position{pos}{}
+
+
+position movementManager::getPos() const
+{
+    return d_position;
+}
+
