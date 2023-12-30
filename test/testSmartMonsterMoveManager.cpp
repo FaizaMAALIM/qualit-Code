bfef76dc94cf3c7a43dc6088da1eea4f093d96d2
Sandra9865 <naitatmane.sarah2004@gmail.com>
2023-12-30 19:28:26 +0100

    tests

--- test/testSmartMonsterMoveManager.cpp
+++ test/testSmartMonsterMoveManager.cpp
@@ -5,6 +5,18 @@
 #include "position.h"
 #include "smartMonsterMoveManager.h"
 
+#include "groundElement.h"
+#include "character.h"
+#include "monster.h"
+#include "smartMonster.h"
+#include "blindMonster.h"
+#include "adventurer.h"
+#include "wall.h"
+#include "amulet.h"
+#include "outside.h"
+#include "door.h"
+#include "money.h"
+
 TEST_CASE("Test de la classe smart monster move manager")
 {
     position p{1,1};
@@ -47,6 +59,60 @@ TEST_CASE("Test de la classe smart monster move manager")
             position posDir = m.directionPosition(8);
             position posAttendue = {p.getLine()+1, p.getColumn()};
             REQUIRE_EQ(posDir,posAttendue);
+        }
+
+    }
+
+    SUBCASE("Test de la méthode vérifiant si le monstre est proche de l'aventurier")
+    {
+        SUBCASE("Test aventurier proche")
+        {
+
+            //construction d'un terrain 3x3
+
+            ground g{3,3};
+
+
+            /*  E | E  | E
+               ---|----|---
+                E | P  | S
+               ---|----|---
+                E | E  | E
+            */
+
+            // ajout de l'aventurier à la case 1,1
+            position pAdv{1,1};
+            auto p = std::make_unique<adventurer>( pAdv);
+            g.addElementToGround(std::move(p));
+
+            //ajout d'un smart monster à la case 1,2
+            position pMonstre{1,2};
+            auto s = std::make_unique<smartMonster>(pMonstre);
+            g.addElementToGround(std::move(s));
+
+
+            smartMonsterMoveManager smrtMv{pMonstre};
+            REQUIRE_EQ(smrtMv.isNearAdventurer(g),true);
+        }
+        SUBCASE("L'aventurier est loin")
+        {
+            //construction d'un terrain 10x10
+
+            ground g{10,10};
+
+            // ajout de l'aventurier à la case 0,0
+            position pAdv{0,0};
+            auto p = std::make_unique<adventurer>(pAdv);
+            g.addElementToGround(std::move(p));
+
+            //ajout d'un smart monster à la case 9,9
+            position pMonstre{9,9};
+            auto s = std::make_unique<smartMonster>(pMonstre);
+            g.addElementToGround(std::move(s));
+
+
+            smartMonsterMoveManager smrtMv{pMonstre};
+            REQUIRE_EQ(smrtMv.isNearAdventurer(g),false);
         }
 
     }
