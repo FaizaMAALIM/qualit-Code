7115df9e74d7e2a1b1b7e1658a14687aa2a90923
sheraDev <scherazade2222@gmail.com>
2023-12-30 09:41:08 +0100

    Ajout classe abstraite gestion mouvements

---
+++ headers/movementManager.h
@@ -0,0 +1,22 @@
+ 
+#ifndef MOVEMENTMANAGER_H_INCLUDED
+#define MOVEMENTMANAGER_H_INCLUDED
+#include "position.h"
+#include "vector"
+#include "ground.h"
+class movementManager
+{
+    public:
+         movementManager(const position&pos);
+        virtual ~movementManager()=default;
+        position getPos() const;
+
+        virtual position
+
+
+  private:
+        position d_position;
+
+};
+
+#endif
