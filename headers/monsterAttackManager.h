f191d2c40b8fb4baddecd18093c3a9dde949c4c8
FaizaMAALIM <maalimfaiza06@gmail.com>
2023-12-31 19:38:05 +0100

    class monster attak

---
+++ headers/monsterAttackManager.h
@@ -0,0 +1,16 @@
+ #ifndef MONSTERATTACKMANAGER_H_INCLUDED
+#define MONSTERATTACKMANAGER_H_INCLUDED
+#include "attackManager.h"
+
+
+class monsterAttackManager : public attackManager
+{
+    public:
+      monsterAttackManager();
+
+
+
+};
+
+
+#endif
---
+++ sources/monsterAttackManager.cpp
@@ -0,0 +1,5 @@
+#include "memory"
+#include "adventurer.h"
+#include "monster.h"
+
+monsterAttackManager::monsterAttackManager(){}
